d6b0090242494a674ec85e244884299fe3745738
Zi90 <edwin0322@gmail.com>
2024-11-13 18:12:01 +0900

    day01

---
+++ src/main/java/com/ezen/spring/controller/BoardController.java
@@ -0,0 +1,38 @@
+package com.ezen.spring.controller;
+
+import com.ezen.spring.domain.BoardVO;
+import com.ezen.spring.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/board/*")
+@RequiredArgsConstructor
+@Slf4j
+@Controller
+public class BoardController {
+    private final BoardService bsv;
+
+    @GetMapping("/register")
+    public String register(){
+        return"/board/register";
+    }
+
+    @PostMapping("/register")
+    public String register(BoardVO boardVO){
+        log.info(">>>> boardVO >>>> {}", boardVO);
+        int isOk = bsv.register(boardVO);
+
+        return "index";
+    }
+
+    @GetMapping("/list")
+    public String list(Model m){
+        m.addAttribute("list", bsv.getList());
+        return"/board/list";
+    }
+}
